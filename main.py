2b5aaca777624ad3bfca3a734e6bddb8379cdbc0
Peyton Creery <44987569+phcreery@users.noreply.github.com>
2019-06-26 12:35:33 -0500

    add list feature and manual input interpreter

--- main.py
+++ main.py
@@ -6,19 +6,56 @@ import tui
 
 screensize = tui.getsize()
 
-def intrusive():
-    # Launches Input and waits for user to complete the Input
-    r = tui.kbdinput("Prompt",screensize,"small","","WASD to Move     E to select    C for backspace    R/F to change KBD     Q to quit")
+
+def getinput():
+    char = tui.getch()
+    if (char == "q"):
+        press = "exit"
+    elif (char == "a"):
+        press = "left"
+    elif (char == "d"):
+        press = "right"    
+    elif (char == "s"):
+        press = "down"
+    elif (char == "w"):
+        press = "up"
+    elif (char == "r"):
+        press = "next"
+    elif (char == "f"):
+        press = "previous"
+    elif (char == "c"):
+        press = "back"
+    elif (char == "e"):
+        press = "select"
+    return(press)
+
+def intrusivekbd():
+    # Launches Input and waits for user to complete the Input.
+    # "defaultinput" sets input interpreter to library default
+    # sizes: small/medium/big/giant
+    r = tui.kbdinput("Prompt",screensize,"big","","WASD to Move     E to select    C for backspace    R/F to change KBD     Q to quit","defaultinput")
 
     print(r)
 
-def unintrusive():
+def unintrusivekbd():
     # Manual keyboard strokes and refreshing of Input
     manually = tui.manualkbdinput()
     manually.sendkbdinput("down")
     manually.sendkbdinput("right")
-    manually.drawkbdinput("Prompt",screensize,"medium","","Unintrusive KBD")
+    manually.sendkbdinput("select")
+    manually.sendkbdtext("asd")
+    s = manually.drawkbdinput("Prompt",screensize,"medium","Unintrusive Keyboard")
+    
+    print(s)
+
+def intrusivelist():
+    #List box with user defined interperetation of input
+    options = ["first","second","third","fourth"]
+    t = tui.listselect("Select one:",screensize,options,"small","tooltip",getinput)
+    print(t)
 
 
-intrusive()
-unintrusive()
+
+intrusivekbd()
+#unintrusivekbd()
+#intrusivelist()
